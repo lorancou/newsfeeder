74be381ebd44d34006774bd7157ce8274ce5c959
Laurent Couvidou <lorancou@free.fr>
2013-02-27 20:51:49 +0100

    Hanging news

--- main.js
+++ main.js
@@ -2,7 +2,23 @@ function main()
 {
 	var ap = document.getElementById("ap");
 		
-	var style = "rotate(" + 45 + "deg)";
+    // run
+    setTimeout("update()", 0.0);
+}
+
+var g_time = 0.0;
+
+function update()
+{
+	var AIM_FPS = 60.0;
+	var MIN_DT = 1000.0 / AIM_FPS;
+	setTimeout("update()", MIN_DT);
+	
+	g_time += MIN_DT;
+	
+	var angle = 90 + Math.sin(g_time * 0.005) * 45;
+
+	var style = "rotate(" + Math.floor(angle) + "deg)";
 
 	ap.style.transform = style;
 	ap.style.WebkitTransform = style + " translateZ(0)"; // Force HW Acceleration
@@ -10,6 +26,9 @@ function main()
 	ap.style.OTransform = style;
 	ap.style.msTransform = style;
 	
+	ap.style.WebkitTransformOrigin = "0% 50%";
+	
 	ap.style.left = "512px";
-	ap.style.top = "512px";
+	ap.style.top = "100px";
+	
 }
