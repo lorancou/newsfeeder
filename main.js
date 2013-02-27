f81def2c363a863ec4f54fcb0a1bdd46659737e9
Laurent Couvidou <lorancou@free.fr>
2013-02-27 21:31:51 +0100

    Cleanup

--- main.js
+++ main.js
@@ -1,34 +1,89 @@
+"use strict";
+
+//------------------------------------------------------------------------------
+// Global constants
+var g_WIDTH;
+var g_HEIGHT;
+
+//------------------------------------------------------------------------------
+// Global variables
+var g_time = 0.0;
+var g_gpe;
+
+//------------------------------------------------------------------------------
+// GPE constructor
+var gpe = function(id)
+{
+	// Clone source
+	var src = document.getElementById("news_1");
+	this.div = src.cloneNode(true);
+	this.div.id = "gpe_" + id;
+
+	// Set visible
+	this.div.style.visibility = "visible";
+
+	// Set transforms anchor
+	this.div.style.transformOrigin = "0% 50%";
+	this.div.style.WebkitTransformOrigin = "0% 50%";
+	this.div.style.MozTransformOrigin = "0% 50%";
+	this.div.style.OTransformOrigin = "0% 50%";
+	this.div.style.msTransformOrigin = "0% 50%";
+
+	// Add to document
+	document.body.appendChild(this.div);
+
+	// Init variables
+	this.timer = 0.0;
+	this.pos = 0;	
+}
+
+//------------------------------------------------------------------------------
+// GPE update
+gpe.prototype.update = function(dt)
+{
+	this.timer += dt;
+	
+	// Compute angle
+	var angle = 90 + Math.sin(this.timer * 0.005) * 45;
+	var style = "rotate(" + Math.floor(angle) + "deg)";
+
+	// Update rotation
+	this.div.style.transform = style;
+	this.div.style.WebkitTransform = style + " translateZ(0)"; // Force HW Acceleration
+	this.div.style.MozTransform = style;
+	this.div.style.OTransform = style;
+	this.div.style.msTransform = style;
+	
+	// Move
+	this.div.style.left = this.pos + "px";
+	this.div.style.top = "100px";		
+}
+
+//------------------------------------------------------------------------------
+// Main
 function main()
 {
-	var ap = document.getElementById("ap");
-		
-    // run
+	// Init "constants"
+	g_WIDTH = document.body.clientWidth;
+	g_HEIGHT = document.body.clientHeight;
+	
+	// TEMP
+	g_gpe = new gpe(0);
+	
+    // Run
     setTimeout("update()", 0.0);
 }
 
-var g_time = 0.0;
-
+//------------------------------------------------------------------------------
+// Loop
 function update()
 {
-	var AIM_FPS = 60.0;
-	var MIN_DT = 1000.0 / AIM_FPS;
-	setTimeout("update()", MIN_DT);
-	
-	g_time += MIN_DT;
-	
-	var angle = 90 + Math.sin(g_time * 0.005) * 45;
+	// Plan next update
+	var FPS = 60.0;
+	var DT = 1000.0 / FPS;
+	setTimeout("update()", DT);
 
-	var style = "rotate(" + Math.floor(angle) + "deg)";
-
-	ap.style.transform = style;
-	ap.style.WebkitTransform = style + " translateZ(0)"; // Force HW Acceleration
-	ap.style.MozTransform = style;
-	ap.style.OTransform = style;
-	ap.style.msTransform = style;
-	
-	ap.style.WebkitTransformOrigin = "0% 50%";
-	
-	ap.style.left = "512px";
-	ap.style.top = "100px";
-	
+	// Update GPEs
+	g_gpe.update(DT);
 }
+
